4c277bc662bb49947d016514ed75cb333922e2e9
Bano Rani <banor3106@gmail.com>
2026-09-13 12:12:38 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "============================================\n";
+    cout << "              HaploPBWT\n";
+    cout << "============================================\n\n";
+
+    cout << "C++ Implementation of the Positional\n";
+    cout << "Burrows-Wheeler Transform (PBWT)\n\n";
+
+    cout << "Based on:\n";
+    cout << "Richard Durbin, 2014\n";
+    cout << "\"Efficient haplotype matching and storage\n";
+    cout << " using the positional Burrows-Wheeler transform\"\n\n";
+
+    cout << "Implemented Algorithms:\n\n";
+
+    cout << "1. BuildPrefixArray\n";
+    cout << "2. BuildPrefixAndDivergenceArrays\n";
+    cout << "3. ReportLongMatches\n";
+    cout << "4. ReportSetMaximalMatches\n";
+    cout << "5. Match new sequence against reference panel\n";
+
+    cout << "\n============================================\n";
+    cout << "Each algorithm is implemented as a separate\n";
+    cout << "C++ program in this repository.\n";
+    cout << "============================================\n";
+
+    return 0;
+}
